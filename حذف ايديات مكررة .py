d0200069776cd9d542fd5c1cfe194a184090e13f
smoksmok2222 <162553816+smoksmok2222@users.noreply.github.com>
2024-03-07 14:46:20 +0300

    Add files via upload

---
+++ حذف ايديات مكررة .py
@@ -0,0 +1,7 @@
+x1F2q =input("\033[92mFILE PATH:") 
+vZ0IY = []
+with open(x1F2q, "r") as eGjL5:
+    vZ0IY = eGjL5.readlines()
+uE4mX = list(set(vZ0IY))
+with open(x1F2q, "w") as Gx8P3:
+    Gx8P3.writelines(uE4mX)
